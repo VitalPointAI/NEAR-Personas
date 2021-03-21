53409abd66aad8ef14cc25a5f1bfc884965a7226
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-03-21 19:46:43 -0400

    express setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('dist/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
